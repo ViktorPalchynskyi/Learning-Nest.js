d1cde7e0bd5b11b6b96d5d1e8622447339d98a97
Viktor <palchinskij19@gmail.com>
2024-09-11 19:16:44 +0300

    11.09.24 - file naming convintions

--- scratch/src/main.ts
+++ scratch/src/main.ts
@@ -1,26 +1,12 @@
-import { Controller, Module, Get } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
 const port = 3000;
-
-@Controller()
-class AppController {
-    @Get()
-    getRootRoute() {
-        return 'hi there!';
-    }
-}
-
-@Module({
-    controllers: [AppController],
-})
-class AppModule {}
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
-    app.listen(port, () => console.log(`Starting server at port ${port}`)
-    )
+    app.listen(port, () => console.log(`Starting server at port ${port}`));
 }
 
 bootstrap();
