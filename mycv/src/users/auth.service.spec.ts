1e03919c3ce78bc7228c51f37f8de65938774606
Viktor <palchinskij19@gmail.com>
2024-09-13 15:23:53 +0300

    13.09.24 - changing mock implementations

--- mycv/src/users/auth.service.spec.ts
+++ mycv/src/users/auth.service.spec.ts
@@ -2,13 +2,19 @@ import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
+import { BadRequestException } from '@nestjs/common';
 
 describe('AuthService', () => {
     let service: AuthService;
+    let fakeUsersService: Partial<UsersService>;
+    const testUser = {
+        email: 'test@test.com',
+        password: '111test111',
+    };
 
     beforeEach(async () => {
         // Create a fake copy of the users service
-        const fakeUsersService: Partial<UsersService> = {
+        fakeUsersService = {
             find: () => Promise.resolve([]),
             create: (email: string, password: string) => Promise.resolve({ id: 1, email, password } as User),
         };
@@ -31,12 +37,19 @@ describe('AuthService', () => {
     });
 
     it('creates a new user with a salted and hashed password', async () => {
-        const user = await service.signup('test@test.com', '111test111');
+        const user = await service.signup(testUser.email, testUser.password);
         const [salt, hash] = user.password.split('.');
 
-        expect(user.password).not.toEqual('111test111');
+        expect(user.password).not.toEqual(testUser.password);
         expect(hash).toBeDefined();
         expect(salt).toBeDefined();
         expect(salt).toHaveLength(16);
+    });
+
+    it('throws an error if user signs up with email that is in use', async () => {
+        const { email, password } = testUser;
+
+        fakeUsersService.find = () => Promise.resolve([{ id: 1, email, password } as User]);
+        await expect(service.signup(email, password)).rejects.toThrow(BadRequestException);
     });
 });
