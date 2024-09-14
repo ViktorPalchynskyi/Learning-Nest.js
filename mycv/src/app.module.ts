e65f80b4ca720e5fa648d0719f41c435dc2d56b5
Viktor <palchinskij19@gmail.com>
2024-09-14 12:18:57 +0300

    14.09.24 - applying dotenv for config

--- mycv/src/app.module.ts
+++ mycv/src/app.module.ts
@@ -7,19 +7,33 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
 import { APP_PIPE } from '@nestjs/core';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const cookieSession = require('cookie-session');
 
 @Module({
     imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: `.env.${process.env.NODE_ENV}`,
+        }),
         UsersModule,
         ReportsModule,
-        TypeOrmModule.forRoot({
-            type: 'sqlite',
-            database: 'db.sqlite',
-            entities: [User, Report],
-            synchronize: true,
+        TypeOrmModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: (config: ConfigService) => ({
+                type: 'sqlite',
+                database: config.get<string>('DB_NAME'),
+                entities: [User, Report],
+                synchronize: true,
+            }),
         }),
+        // TypeOrmModule.forRoot({
+        //     type: 'sqlite',
+        //     database: 'db.sqlite',
+        //     entities: [User, Report],
+        //     synchronize: true,
+        // }),
     ],
     controllers: [AppController],
     providers: [
