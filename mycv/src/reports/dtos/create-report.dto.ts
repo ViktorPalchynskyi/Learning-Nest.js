cbfc7e6703255715b880bf81caeb4424ccc910be
Viktor <palchinskij19@gmail.com>
2024-09-14 13:27:38 +0300

    14.09.24 - receining report creation requests

--- mycv/src/reports/dtos/create-report.dto.ts
+++ mycv/src/reports/dtos/create-report.dto.ts
@@ -1,7 +1,9 @@
-import { IsString, IsNumber } from 'class-validator';
+import { IsString, IsNumber, Min, Max, IsLatitude, IsLongitude } from 'class-validator';
 
 export class CreateReportDto {
     @IsNumber()
+    @Min(0)
+    @Max(1000000)
     price: number;
 
     @IsString()
@@ -11,14 +13,18 @@ export class CreateReportDto {
     model: string;
 
     @IsNumber()
+    @Min(1930)
+    @Max(2050)
     year: number;
 
-    @IsNumber()
+    @IsLongitude()
     lng: number;
 
-    @IsNumber()
+    @IsLatitude()
     lat: number;
 
     @IsNumber()
+    @Min(0)
+    @Max(1000000)
     mileage: number;
 }
