bb24ec6b0dde53098a697bcb90bf554e6df3c2a7
Viktor <palchinskij19@gmail.com>
2024-09-14 16:16:18 +0300

    14.09.24 - assigning currentUser with a Midlleware

--- mycv/src/reports/reports.controller.ts
+++ mycv/src/reports/reports.controller.ts
@@ -7,6 +7,7 @@ import { User } from 'src/users/user.entity';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { ReportDto } from './dtos/report.dto';
 import { ApproveReportDto } from './dtos/approve-report.dto';
+import { AdminGuard } from 'src/guards/admin.guard';
 
 @Controller('reports')
 export class ReportsController {
@@ -19,6 +20,7 @@ export class ReportsController {
         return this.reportsService.create(body, user);
     }
 
+    @UseGuards(AdminGuard)
     @Patch('/:id')
     approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
         return this.reportsService.changeApproval(id, body.approved);
