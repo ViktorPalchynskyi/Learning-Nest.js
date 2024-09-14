21f8cbc9660d4350cc47a6132f6cdf1c1573543b
Viktor <palchinskij19@gmail.com>
2024-09-14 13:21:37 +0300

    14.09.24 - a dto for reports creation

---
+++ mycv/src/reports/dtos/create-report.dto.ts
@@ -0,0 +1,24 @@
+import { IsString, IsNumber } from 'class-validator';
+
+export class CreateReportDto {
+    @IsNumber()
+    price: number;
+
+    @IsString()
+    make: string;
+
+    @IsString()
+    model: string;
+
+    @IsNumber()
+    year: number;
+
+    @IsNumber()
+    lng: number;
+
+    @IsNumber()
+    lat: number;
+
+    @IsNumber()
+    mileage: number;
+}
--- mycv/src/reports/reports.controller.ts
+++ mycv/src/reports/reports.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+    @Post
+    createReport(@Body() body) {
+
+    }
+}
