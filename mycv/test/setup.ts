096e64039a5f824e18b29deba57820ea2bc19c87
Viktor <palchinskij19@gmail.com>
2024-09-14 12:39:24 +0300

    14.09.24 - global before each

---
+++ mycv/test/setup.ts
@@ -0,0 +1,10 @@
+import { rm } from 'node:fs/promises';
+import { join } from 'node:path';
+
+global.beforeEach(async () => {
+    try {
+        await rm(join(__dirname, '..', 'test.sqlite'));
+    } catch (error) {
+        console.log(error);
+    }
+});
