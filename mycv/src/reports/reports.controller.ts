5c2f1975c3e4e8104157f322846fec2d45352c00
Viktor <palchinskij19@gmail.com>
2024-09-14 16:35:53 +0300

    14.09.24 - validation query string

--- mycv/src/reports/reports.controller.ts
+++ mycv/src/reports/reports.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, Query, UseGuards } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from 'src/guards/auth.guard';
@@ -8,10 +8,14 @@ import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { ReportDto } from './dtos/report.dto';
 import { ApproveReportDto } from './dtos/approve-report.dto';
 import { AdminGuard } from 'src/guards/admin.guard';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Controller('reports')
 export class ReportsController {
     constructor(private reportsService: ReportsService) {}
+
+    @Get()
+    getEstimate(@Query() query: GetEstimateDto) {}
 
     @UseGuards(AuthGuard)
     @Serialize(ReportDto)
